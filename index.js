840c4925b96b1f3a0c6d09235eaa80c2e7ae548b
Jessica Schmidt <jessyka_sl@hotmail.com>
2023-04-28 21:34:09 -0300

    Programação Orientada a Objetos

---
+++ index.js
@@ -0,0 +1,50 @@
+/*
+const pessoa ={
+    nome: "Jéssica",
+    idade: 30,
+    email: "jessyka_sl@hotmail.com",
+}
+console.log("Nome: ",pessoa.nome,"\n","Idade: ", pessoa.idade);
+console.log("E-mail: ", pessoa.email);
+
+
+const bicicleta = {
+    cor: "Azul",
+    marchas: 21,
+    estilo: "speed",
+}
+console.log(bicicleta["estilo"], bicicleta["cor"], bicicleta["marchas"]);
+
+//Alterando valores
+
+bicicleta.marchas = 18;
+console.log(bicicleta.marchas);
+
+pessoa.email = "senac@senac.com";
+console.log(pessoa.email);
+*/
+
+//Exercicios
+/*
+const filme = {
+    direcao: "Sony Films",
+    nomeFilme: "A Vida é Bela e Amarela",
+    ano: 2023,
+    elenco:['Juliana Paes', 'Caio Castro','Suzana Vieira','Glória Menezes'],
+    assistiu: "Não",
+}
+console.log('Filme: ', filme.nomeFilme);
+console.log('Ano: ', filme.ano);
+console.log('Direção: ', filme.direcao);
+console.log('Elenco: ', filme.elenco[0] + '\n'+ filme.elenco[1]+'\n'+ filme.elenco[2]+'\n'+ filme.elenco[3]);  
+console.log('Você já assistiu este filme: ', filme["assistiu"]);
+*/
+
+//Exercicio 2
+
+const pessoa = {
+    nome: "Alice",
+    idade: 23,
+    generoMusical: "Pop Rock."
+}
+console.log("O nome da pessoa é ",pessoa.nome+", ela tem ",pessoa.idade,"anos e gosta muito de ",pessoa.generoMusical);
